778f8811d98942607d53a8b1faae73abed51e249
Neha Sharma <i.nehaa98@gmail.com>
2023-07-05 21:13:38 +0530

    CRUD Functionality Implemented

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -99,19 +99,19 @@ const BootcampSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
-    user: {
+    /* user: {
       type: mongoose.Schema.ObjectId,
       ref: "User",
       required: true,
-    },
-  },
+    }, */
+  } /* ,
   {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
-  }
+  } */
 );
 
-// Create bootcamp slug from the name
+/* // Create bootcamp slug from the name
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
@@ -150,5 +150,5 @@ BootcampSchema.virtual("courses", {
   foreignField: "bootcamp",
   justOne: false,
 });
-
+ */
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
