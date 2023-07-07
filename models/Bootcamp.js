2c32b6b62d9c0abaae372784437c4e568116600e
Neha Sharma <i.nehaa98@gmail.com>
2023-07-07 21:34:47 +0530

    Error Handling Done

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-//const slugify = require("slugify");
+const slugify = require("slugify");
 //const geocoder = require("../utils/geocoder");
 
 const BootcampSchema = new mongoose.Schema(
@@ -111,13 +111,12 @@ const BootcampSchema = new mongoose.Schema(
   } */
 );
 
-/* // Create bootcamp slug from the name
+// Create bootcamp slug from the name
 BootcampSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
-
-// Geocode & create location field
+/*// Geocode & create location field
 BootcampSchema.pre("save", async function (next) {
   const loc = await geocoder.geocode(this.address);
   this.location = {
